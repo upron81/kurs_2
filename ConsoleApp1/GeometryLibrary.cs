9930bc301732c8e580d3d8da9562fa9240bcfee6
upron81 <77348627+upron81@users.noreply.github.com>
2023-09-27 14:57:15 +0300

    Add files via upload

---
+++ ConsoleApp1/GeometryLibrary.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace GeometryLibrary
+{
+    /// <summary>
+    /// Представляет квадрат с указанной длиной стороны.
+    /// </summary>
+    public class Square
+    {
+        private double sideLength;
+        private double x;
+        private double y;
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса Square с указанной длиной стороны.
+        /// </summary>
+        /// <param name="sideLength">Длина стороны квадрата.</param>
+        public Square(double sideLength, double x, double y)
+        {
+            this.sideLength = sideLength;
+            this.x = x;
+            this.y = y;
+            if (!IsValid())
+            {
+                throw new InvalidOperationException("Квадрат недопустимой формы");
+            }
+        }
+
+        /// <summary>
+        /// Проверяет, является ли квадрат допустимой формы.
+        /// </summary>
+        /// <returns>True, если квадрат допустимой формы, в противном случае - False.</returns>
+        public bool IsValid()
+        {
+            return sideLength > 0;
+        }
+
+        /// <summary>
+        /// Вычисляет площадь квадрата.
+        /// </summary>
+        /// <returns>Площадь квадрата.</returns>
+        /// <exception cref="InvalidOperationException">Вызывается, если квадрат недопустимой формы.</exception>
+        public double CalculateArea()
+        {
+            return sideLength * sideLength;
+        }
+
+        /// <summary>
+        /// Вычисляет периметр квадрата.
+        /// </summary>
+        /// <returns>Периметр квадрата.</returns>
+        /// <exception cref="InvalidOperationException">Вызывается, если квадрат недопустимой формы.</exception>
+        public double CalculatePerimeter()
+        {
+            return 4 * sideLength;
+        }
+
+        /// <summary>
+        /// Проверяет, содержит ли квадрат указанную точку.
+        /// </summary>
+        /// <param name="x">Координата X точки.</param>
+        /// <param name="y">Координата Y точки.</param>
+        /// <returns>True, если точка находится внутри квадрата, в противном случае - False.</returns>
+        /// <exception cref="InvalidOperationException">Вызывается, если квадрат недопустимой формы.</exception>
+        public bool ContainsPoint(double x, double y)
+        {
+            return x-this.x > 0 && x-this.x < sideLength && y-this.y > 0 && y-this.y < sideLength;
+        }
+
+        /// <summary>
+        /// Проверяет, находится ли точка на границе квадрата.
+        /// </summary>
+        /// <param name="x">Координата X точки.</param>
+        /// <param name="y">Координата Y точки.</param>
+        /// <returns>True, если точка находится на границе квадрата, в противном случае - False.</returns>
+        /// <exception cref="InvalidOperationException">Вызывается, если квадрат недопустимой формы.</exception>
+        public bool IsOnBoundary(double x, double y)
+        {
+            return (x-this.x == 0 || x - this.x == sideLength) && (y - this.y >= 0 && y - this.y <= sideLength) ||
+                   (y - this.y == 0 || y - this.y == sideLength) && (x - this.x >= 0 && x - this.x <= sideLength);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using GeometryLibrary;
+
+/// <summary>
+/// Это главный класс приложения, который предоставляет возможность пользователю вводить данные
+/// и выполнять операции с квадратом.
+/// </summary>
+class Program
+{
+    /// <summary>
+    /// Точка входа в приложение.
+    /// </summary>
+    static void Main()
+    {
+        Console.WriteLine("Введите длину стороны квадрата:");
+        double sideLength = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите x:");
+        double sx = double.Parse(Console.ReadLine());
+        Console.WriteLine("Введите y:");
+        double sy= double.Parse(Console.ReadLine());
+        // Создаем экземпляр квадрата на основе введенной длины стороны.
+        Square square;
+
+        try
+        {
+            square = new Square(sideLength, sx, sy);
+            Console.WriteLine($"Площадь квадрата: {square.CalculateArea()}");
+            Console.WriteLine($"Периметр квадрата: {square.CalculatePerimeter()}");
+
+            Console.WriteLine("Введите координаты точки для проверки ее принадлежности квадрату:");
+            Console.Write("X: ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Y: ");
+            double y = double.Parse(Console.ReadLine());
+
+            if (square.ContainsPoint(x, y))
+            {
+                Console.WriteLine($"Точка ({x}, {y}) принадлежит квадрату.");
+            }
+            else if (square.IsOnBoundary(x, y))
+            {
+                Console.WriteLine($"Точка ({x}, {y}) находится на границе квадрата.");
+            }
+            else
+            {
+                Console.WriteLine($"Точка ({x}, {y}) не принадлежит квадрату.");
+            }
+        }
+        catch {
+            Console.WriteLine("Недопустимая длина стороны квадрата.");
+        }
+    }
+}
