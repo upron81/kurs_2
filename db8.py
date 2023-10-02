3a5b2162f277d0d0e46d9c1c6c434b7429686611
upron81 <77348627+upron81@users.noreply.github.com>
2023-10-02 12:28:45 +0300

    Add files via upload

---
+++ db8.py
@@ -0,0 +1,41 @@
+import os
+
+class Db:
+    filename = ''
+
+    def __init__(self, model):
+        self.model = model
+        self.data: list[model] = []
+
+    def set_file(self, filename):
+        self.filename = filename
+        
+    def get_data(self):
+        if os.path.exists(self.filename): 
+            with open(self.filename, 'r') as db_file:
+                self.data = self.model.schema().loads(db_file.read(), many=True)
+        else:
+            self.data = []
+    
+    def set_data(self):
+        with open(self.filename, 'w') as db_file:
+            db_file.write(self.model.schema().dumps(self.data, many=True))    
+    
+    def create_model(self):
+        new_model = Event()
+        func_convert = {"<class 'int'>": int, "<class 'float'>": float, "<class 'bool'>": bool, "<class 'str'>": str}
+        for field in new_model.__dataclass_fields__:
+            new_model.__dict__[field] = func_convert[str(type(new_model.__getattribute__(field)))](input(f'Введите {field}: '))
+        self.data.append(new_model)
+            
+    def delete_model(self, index: int):
+        try:
+           del self.data[index]
+        except:
+            print(f"Нет элемента с индексом {index}!")
+
+    def update_model(self, index: int, field: str, new_value):
+        try:
+            self.data[index].__dict__[field] = new_value
+        except:
+            print('Ошибка')
