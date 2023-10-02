8c44c5ce9b965b829f820179677aa421ffde0f94
upron81 <77348627+upron81@users.noreply.github.com>
2023-10-02 12:28:09 +0300

    Update python_lab8.py

--- python_lab8.py
+++ python_lab8.py
@@ -1,6 +1,6 @@
-import os
 from dataclasses import dataclass
 from dataclasses_json import dataclass_json
+from db8 import Db
 
 
 @dataclass_json
@@ -13,61 +13,17 @@ class Event:
     number_people: int = 0
     lastnames: str = 'фамилию'
 
-
-class Db:
-    model = Event
-    data: list[model] = []
-    filename = ''
-    db_file = ''
-
-    def set_file(self, filename):
-        self.filename = filename
-        
-    def get_data(self):
-        with open(self.filename, 'r') as db_file:
-            self.db_file = self.filename
-            self.data = self.model.schema().loads(self.data, many=True)
-    
-    def set_data(self):
-        if not os.path.exists(self.filename): 
-            open(self.filename, 'x').close()
-        with open(self.db_file, 'w') as db_file:
-            db_file.write(self.model.schema().dumps(self.data, many=True))    
-    
-    def create_model(self):
-        new_model = Event()
-        func_convert = {"<class 'int'>": int, "<class 'float'>": float, "<class 'bool'>": bool, "<class 'str'>": str}
-        for field in new_model.__dataclass_fields__:
-            new_model.__dict__[field] = func_convert[str(type(new_model.__getattribute__(field)))](input(f'Введите {field}: '))
-        self.data.append(new_model)
-            
-    def delete_model(self, index: int):
-        try:
-           del self.data[index]
-        except:
-            print(f"Нет элемента с индексом {index}!")
-
-    def update_model(self, index: int, field: str, new_value):
-        try:
-            self.data[index].__dict__[field] = new_value
-        except:
-            print('Ошибка')
-
-
 def main():
-    db = Db()
-    
+    db = Db(Event)
     while True:
         if db.filename:
             db.get_data()
-            print('Load data')
-            print(type(db.data))
-            print('END DATA')
             
         command = [i for i in input(">>> ").split() if i]
         match command:
             case ['db', filename]:
-                db.set_data()
+                if db.filename:
+                    db.set_data()
                 db.set_file(filename)
                 continue
             case ['read']:
@@ -79,11 +35,11 @@ def main():
             case ['update', index, field]:
                 db.update_model(int(index), field)
             case ['exit']:
+                db.set_data()
                 exit()
 
-        if db.db_file:
+        if db.filename:
             db.set_data()
-            print("Save data")
         
         
 if __name__ == "__main__":
